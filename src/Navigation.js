eb80713471113010429fa2b391c6d96c995dbb83
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-10-01 10:28:46 +0300

    .

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <nav className="flex p-4 text-center gap-4 flex-col border-b-slate-400 border-b-2 sm:flex-row sm:justify-between">
-      <h1 className="text-3xl font-medium text-rose-500">The Blog Posts</h1>
+      <h1 className="text-3xl font-medium text-rose-500">RoboPosts</h1>
       <div className=" flex flex-col  gap-6 font-medium sm:flex-row">
         <Link className="border-b-slate-400 border-b-2 sm:border-none" to="/">
           Home
